dc5b8161caa7c9cc7fcb4f18d371fb3cd17e54dd
123lxm456 <2993626549@qq.com>
2026-01-28 17:24:20 +0800

    Clear root directory

--- 1000/LC 0342.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public boolean isPowerOfFour(int n) {
-        while(n != 0 && (n % 4 == 0))
-        {
-            n /= 4;
-        }
-        return n == 1;
-    }
-}
--- 1000/README.md
+++
@@ -1,2 +0,0 @@
-# LeetCode
-my leetcode record
